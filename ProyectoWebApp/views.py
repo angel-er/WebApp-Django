0c683dee2821b1436ff0fb9fa39cd0373b4b23e6
angel-er <a.encinasrm@gmail.com>
2022-05-16 17:34:43 -0300

    Create Servicio app

--- ProyectoWebApp/views.py
+++ ProyectoWebApp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, HttpResponse
+from servicios.models import Servicio
 
 # Create your views here.
 
@@ -8,7 +9,8 @@ def home(request):
 
 
 def servicios(request):
-    return render(request, 'ProyectoWebApp/servicios.html')
+    servicios = Servicio.objects.all()
+    return render(request, 'ProyectoWebApp/servicios.html', {'servicios': servicios})
 
 
 def tienda(request):
