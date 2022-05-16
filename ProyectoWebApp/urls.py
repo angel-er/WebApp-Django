01b185286419a3105442a3576f1a7422425f2c72
angel-er <a.encinasrm@gmail.com>
2022-05-16 17:45:10 -0300

    Service module restructuring

--- ProyectoWebApp/urls.py
+++ ProyectoWebApp/urls.py
@@ -7,7 +7,6 @@ from ProyectoWebApp import views
 
 urlpatterns = [
     path('', views.home, name='Home'),
-    path('servicios/', views.servicios, name='Servicios'),
     path('tienda/', views.tienda, name='Tienda'),
     path('blog/', views.blog, name='Blog'),
     path('contacto/', views.contacto, name='Contacto'),
@@ -15,6 +14,6 @@ urlpatterns = [
 
 # url = 'ProyectoWebApp/' + settings.MEDIA_URL
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-print(settings.MEDIA_ROOT)
-print(settings.MEDIA_URL)
-print(urlpatterns)
+# print(settings.MEDIA_ROOT)
+# print(settings.MEDIA_URL)
+# print(urlpatterns)
