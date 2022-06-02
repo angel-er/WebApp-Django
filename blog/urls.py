a009ad63364dabc8553eb154a5b4c7566b37103b
angel-er <a.encinasrm@gmail.com>
2022-06-02 14:34:13 -0300

    Feature: Category template creation

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('blog/', views.blog, name='Blog'),
+    path('', views.blog, name='Blog'),
+    path('categoria/<int:categoria_id>/', views.categoria, name='Categoria'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
-
+from .models import Post, Categoria
 # Create your views here.
 
 
 def blog(request):
-    return render(request, 'blog/blog.html')
+    posts = Post.objects.all()
+    return render(request, 'blog/blog.html', {'posts': posts})
+
+
+def categoria(request, categoria_id):
+    categoria = Categoria.objects.get(id=categoria_id)
+    posts = Post.objects.filter(categorias=categoria)
+    print(categoria)
+    return render(request, 'blog/categoria.html', {'categoria': categoria, 'posts': posts})
